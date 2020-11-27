8402801599cb85d4e7cfec77dd8ccdf47700ff1a
Marcelo García Prieto <magarciaprieto@gmail.com>
2020-11-27 12:49:21 -0300

    Revision 1 Button after PR

--- src/Button.js
+++ src/Button.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import styles from './Button.module.css';
 
-export default function Button(props) {
-  const color = props.color
-  const label = props.label
-  
+export default function Button({color, children, onClick}) {
+  const styleButton = `${styles.button} `;
+  const style = (color === 'red') ? styleButton + styles.red : styleButton + styles.blue;
+
   return (
-    <button style={{backgroundColor:color}} className={styles.button}>
-      {label}
+    <button className={style} onClick={onClick}>
+      {children}
     </button>
   );
 }
