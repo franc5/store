ffb7d812a1eb0b34753ce37eb7cdca9d55b24c8d
Francisco Costa <francosta@protonmail.com>
2021-01-18 21:51:28 -0300

    Add an example story

---
+++ src/components/Button/index.stories.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Button from './';
+
+export default {
+  title: 'Button',
+  component: Button,
+};
+
+const Template = (args) => <Button {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  children: 'Button',
+};
