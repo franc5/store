036881b734ee2766e122f7051b671c0f55465fb9
Marcelo García Prieto <magarciaprieto@gmail.com>
2021-01-22 23:36:33 -0300

    Add ItemList Component

---
+++ src/components/ItemList/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import styles from './index.module.css';
+import PropTypes from 'prop-types';
+
+
+function ItemList ({image, title, price}) {
+
+  return (
+    <div className={styles.itemListContainer}>
+      <div className={styles.imageContainer}>
+        <img src={image} alt={title}></img>
+      </div>
+      <p title={title} className={styles.itemListTitle}> {title} </p>
+      <p className={styles.itemListPrice}> {price} </p>
+    </div>
+  );
+}
+
+// Set the propTypes for the component
+ItemList.propTypes = {
+  image: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  price: PropTypes.string.isRequired,
+};
+
+export default ItemList
