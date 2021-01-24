a930d42639344d754da89779ebb38595e493b781
Marcelo García Prieto <magarciaprieto@gmail.com>
2021-01-24 00:44:53 -0300

    Checkbox Stories

---
+++ src/components/Checkbox/index.stories.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Checkbox from './';
+
+export default {
+  title: 'Checkbox',
+  component: Checkbox,
+};
+
+const Template = (args) => <Checkbox {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  label: 'Label',
+};
