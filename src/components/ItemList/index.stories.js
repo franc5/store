64b38833f29cf8263f53c2687c7ed63f62cc7ec1
Marcelo García Prieto <magarciaprieto@gmail.com>
2021-01-29 19:36:59 -0300

    R1 after PR of Stories Issue: Add ItemList Stories

---
+++ src/components/ItemList/index.stories.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ItemList from './';
+
+export default {
+  title: 'ItemList',
+  component: ItemList,
+};
+
+const Template = ({...args}) => <ItemList {...args} />
+
+export const Default = Template.bind({});
+
+Default.args = {
+  image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRFd9QvX_T9YmOMiQN144FIgdQgzwHYmRcx0A&usqp=CAU',
+  title: 'Husqvarna Svartpilen 401 50% 18 Cuotas Tasa 0% Palermo Bikes',
+  price: 4500,
+};
